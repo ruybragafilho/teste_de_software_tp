047a774c01c35aff68eb884978955fe9dbb1c4fc
Ruy Braga Filho <ruybraga@vermelho.grad.dcc.ufmg.br>
2022-11-08 18:07:27 -0300

    Implementacao do KingTest e correcao do RookTest

---
+++ src/test/java/com/chessgame/chess/pieces/KingTest.java
@@ -0,0 +1,185 @@
+// Teste da classe King, usando o framework de testes junit
+// Autor: Ruy Braga Filho
+
+package test.java.com.chessgame.chess.pieces;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import main.java.com.chessgame.boardGame.Board;
+import main.java.com.chessgame.boardGame.Piece;
+import main.java.com.chessgame.boardGame.Position;
+import main.java.com.chessgame.chess.Color;
+import main.java.com.chessgame.chess.pieces.King;
+
+
+public class KingTest {
+
+	Board board;
+	Piece king1;
+
+
+	@Before
+	public void init() {
+
+        // Índices das linhas: de 0 à 7 
+        // Índices das colunas: de 0 à 7 
+		Board board = new Board(8, 8);		
+
+		Piece king1 = new King(board, Color.WHITE);
+        Position position1 = new Position(3, 3);		
+		board.placePiece(king1, position1);		
+	}
+
+
+    // Tenta movimentar o rei, uma posição, na diagonal superior à esquerda,
+    // o que é permitido pelas regras do xadrez. A posição está vazia.
+	@Test
+	public void testDiagonalSuperiorEsquerda() {
+	
+        assertTrue( king1.possibleMove(new Position(2, 2)) );
+	}
+
+    // Tenta movimentar o rei, uma posição, na diagonal superior à direita,
+    // o que é permitido pelas regras do xadrez. A posição está vazia.   
+	@Test
+	public void testDiagonalSuperiorDireita() {
+
+        assertTrue( king1.possibleMove(new Position(2, 4)) );
+	}
+
+    // Tenta movimentar o rei, uma posição, na diagonal inferior à esquerda,
+    // o que é permitido pelas regras do xadrez. A posição está vazia.   
+	@Test
+	public void testDiagonalInferiorEsquerda() {
+		
+        assertTrue( king1.possibleMove(new Position(4, 2)) );
+	}
+    
+    // Tenta movimentar o rei, uma posição, na diagonal inferior à direita,
+    // o que é permitido pelas regras do xadrez. A posição está vazia.   
+	@Test
+	public void testDiagonalInferiorDireita() {
+		
+        assertTrue( king1.possibleMove(new Position(4, 4)) );
+	}    
+
+
+    // Tenta movimentar o rei, uma posição, na horizontal para a direita, 
+    // o que é permitido pelas regras do xadrez. A posição está vazia.
+	@Test
+	public void testHorizontalDireita() {
+		
+        assertTrue( king1.possibleMove(new Position(3, 4)) );
+	}    
+
+    // Tenta movimentar o rei, uma posição, na horizontal para a esquerda, 
+    // o que é permitido pelas regras do xadrez. A posição está vazia.
+	@Test
+	public void testHorizontalEsquerda() {;
+		
+        assertTrue( king1.possibleMove(new Position(3, 2)) );
+	}    
+
+    // Tenta movimentar o rei, uma posição, na vertical para cima, 
+    // o que é permitido pelas regras do xadrez. A posição está vazia.
+	@Test
+	public void testVerticalCima() {
+		
+        assertTrue( king1.possibleMove(new Position(2, 3)) );
+	}    
+
+    // Tenta movimentar o rei, uma posição, na vertical para baixo, 
+    // o que é permitido pelas regras do xadrez. A posição está vazia.
+	@Test
+	public void testVerticalBaixo() {
+		
+        assertTrue( king1.possibleMove(new Position(4, 3)) );
+	}       
+
+
+
+    // Tenta movimentar o rei, duas posições, na diagonal superior à esquerda,
+    // o que não é permitido pelas regras do xadrez. A posição está vazia.
+	@Test
+	public void testDiagonalSuperiorEsquerda() {
+
+        assertFalse( king1.possibleMove(new Position(1, 1)) );
+	}
+
+    // Tenta movimentar o rei, duas posições, na diagonal superior à direita,
+    // o que não é permitido pelas regras do xadrez. A posição está vazia.   
+	@Test
+	public void testDiagonalSuperiorDireita() {
+
+        assertFalse( king1.possibleMove(new Position(1, 5)) );
+	}
+
+    // Tenta movimentar o rei, duas posições, na diagonal inferior à esquerda,
+    // o que não é permitido pelas regras do xadrez. A posição está vazia.   
+	@Test
+	public void testDiagonalInferiorEsquerda() {
+	
+        assertFalse( king1.possibleMove(new Position(5, 1)) );
+	}
+    
+    // Tenta movimentar o rei, duas posições, na diagonal inferior à direita,
+    // o que não é permitido pelas regras do xadrez. A posição está vazia.   
+	@Test
+	public void testDiagonalInferiorDireita() {
+	
+        assertFalse( king1.possibleMove(new Position(5, 5)) );
+	}    
+
+
+    // Tenta movimentar o rei, duas posições, na horizontal para a direita, 
+    // o que não é permitido pelas regras do xadrez. A posição está vazia.
+	@Test
+	public void testHorizontalDireita() {
+	
+        assertFalse( king1.possibleMove(new Position(3, 5)) );
+	}    
+
+
+    // Tenta movimentar o rei, duas posições, na horizontal para a esquerda, 
+    // o que não é permitido pelas regras do xadrez. A posição está vazia.
+	@Test
+	public void testHorizontalEsquerda() {
+		
+        assertFalse( king1.possibleMove(new Position(3, 1)) );
+	}    
+
+
+    // Tenta movimentar o rei, duas posições, na vertical para cima, 
+    // o que não é permitido pelas regras do xadrez. A posição está vazia.
+	@Test
+	public void testVerticalCima() {
+		
+        assertFalse( king1.possibleMove(new Position(1, 3)) );
+	}    
+
+
+    // Tenta movimentar o rei, duas posições, na vertical para baixo, 
+    // o que não é permitido pelas regras do xadrez. A posição está vazia.
+	@Test
+	public void testVerticalBaixo() {
+		
+        assertFalse( king1.possibleMove(new Position(5, 3)) );
+	}       
+
+	
+    // Tenta atacar um rei inimigo, que está uma posição acima, na vertical.
+	@Test
+	public void testAtacarTorreInimiga() {
+
+		Piece king2 = new King(board, Color.BLACK );
+		Position position2 = new Position(2, 3);        
+		board.placePiece(king2, position2);        
+		
+		assertTrue( king1.possibleMove(new Position(2, 3)) );
+	}
+
+}
--- src/test/java/com/chessgame/chess/pieces/RookTest.java
+++ src/test/java/com/chessgame/chess/pieces/RookTest.java
@@ -18,18 +18,27 @@ import main.java.com.chessgame.chess.pieces.Rook;
 
 public class RookTest {
 
+	Board board;
+	Piece rook1;
+
+
+	@Before
+	public void init() {
+
+        // Índices das linhas: de 0 à 7 
+        // Índices das colunas: de 0 à 7 
+		Board board = new Board(8, 8);		
+
+		Piece rook1 = new Rook(board, Color.WHITE);
+        Position position1 = new Position(3, 3);		
+		board.placePiece(rook1, position1);		
+	}
+
+
     // Tenta movimentar uma torre para a diagonal superior à esquerda,
     // o que não é permitido pelas regras do xadrez. A posição está vazia.
 	@Test
 	public void testDiagonalSuperiorEsquerda() {
-
-        // Índices das linhas: de 0 à 7 
-        // Índices das colunas: de 0 à 7 
-		Board board = new Board(8, 8);
-
-		Piece rook1 = new Rook(board, Color.WHITE);
-        Position position1 = new Position(3, 3);		
-		board.placePiece(rook1, position1);
 		
         assertFalse( rook1.possibleMove(new Position(2, 2)) );
 	}
@@ -38,14 +47,6 @@ public class RookTest {
     // o que não é permitido pelas regras do xadrez. A posição está vazia.
 	@Test
 	public void testDiagonalSuperiorDireita() {
-
-        // Índices das linhas: de 0 à 7 
-        // Índices das colunas: de 0 à 7 
-		Board board = new Board(8, 8);
-
-		Piece rook1 = new Rook(board, Color.WHITE);
-        Position position1 = new Position(3, 3);		
-		board.placePiece(rook1, position1);
 		
         assertFalse( rook1.possibleMove(new Position(2, 4)) );
 	}
@@ -54,14 +55,6 @@ public class RookTest {
     // o que não é permitido pelas regras do xadrez. A posição está vazia.
 	@Test
 	public void testDiagonalInferiorEsquerda() {
-
-        // Índices das linhas: de 0 à 7 
-        // Índices das colunas: de 0 à 7 
-		Board board = new Board(8, 8);
-
-		Piece rook1 = new Rook(board, Color.WHITE);
-        Position position1 = new Position(3, 3);		
-		board.placePiece(rook1, position1);
 		
         assertFalse( rook1.possibleMove(new Position(4, 2)) );
 	}
@@ -70,14 +63,6 @@ public class RookTest {
     // o que não é permitido pelas regras do xadrez. A posição está vazia.
 	@Test
 	public void testDiagonalInferiorDireita() {
-
-        // Índices das linhas: de 0 à 7 
-        // Índices das colunas: de 0 à 7 
-		Board board = new Board(8, 8);
-
-		Piece rook1 = new Rook(board, Color.WHITE);
-        Position position1 = new Position(3, 3);		
-		board.placePiece(rook1, position1);
 		
         assertFalse( rook1.possibleMove(new Position(4, 4)) );
 	}    
@@ -87,82 +72,38 @@ public class RookTest {
     // o que é permitido pelas regras do xadrez. A posição está vazia.
 	@Test
 	public void testHorizontalDireita() {
-
-        // Índices das linhas: de 0 à 7 
-        // Índices das colunas: de 0 à 7 
-		Board board = new Board(8, 8);
-
-		Piece rook1 = new Rook(board, Color.WHITE);
-        Position position1 = new Position(3, 3);		
-		board.placePiece(rook1, position1);
 		
         assertTrue( rook1.possibleMove(new Position(3, 4)) );
 	}    
-
 
     // Tenta movimentar uma torre na horizontal para a esquerda, 
     // o que é permitido pelas regras do xadrez. A posição está vazia.
 	@Test
 	public void testHorizontalEsquerda() {
-
-        // Índices das linhas: de 0 à 7 
-        // Índices das colunas: de 0 à 7 
-		Board board = new Board(8, 8);
-
-		Piece rook1 = new Rook(board, Color.WHITE);
-        Position position1 = new Position(3, 3);		
-		board.placePiece(rook1, position1);
 		
         assertTrue( rook1.possibleMove(new Position(3, 2)) );
 	}    
-
 
     // Tenta movimentar uma torre na vertical para cima, 
     // o que é permitido pelas regras do xadrez. A posição está vazia.
 	@Test
 	public void testVerticalCima() {
-
-        // Índices das linhas: de 0 à 7 
-        // Índices das colunas: de 0 à 7 
-		Board board = new Board(8, 8);
-
-		Piece rook1 = new Rook(board, Color.WHITE);
-        Position position1 = new Position(3, 3);		
-		board.placePiece(rook1, position1);
 		
         assertTrue( rook1.possibleMove(new Position(2, 3)) );
 	}    
-
 
     // Tenta movimentar uma torre na vertical para baixo, 
     // o que é permitido pelas regras do xadrez. A posição está vazia.
 	@Test
 	public void testVerticalBaixo() {
-
-        // Índices das linhas: de 0 à 7 
-        // Índices das colunas: de 0 à 7 
-		Board board = new Board(8, 8);
-
-		Piece rook1 = new Rook(board, Color.WHITE);
-        Position position1 = new Position(3, 3);		
-		board.placePiece(rook1, position1);
 		
         assertTrue( rook1.possibleMove(new Position(4, 3)) );
 	}       
-
 
 	
     // Tenta atacar uma torre inimiga, que está uma posição acima, na vertical.
 	@Test
 	public void testAtacarTorreInimiga() {
-
-        // Índices das linhas: de 0 à 7 
-        // Índices das colunas: de 0 à 7         
-		Board board = new Board(8, 8);
-
-		Piece rook1 = new Rook(board, Color.WHITE);
-        Position position1 = new Position(3, 3);
-		board.placePiece(rook1, position1);
 
 		Piece rook2 = new Rook(board, Color.BLACK );
 		Position position2 = new Position(2, 3);        
