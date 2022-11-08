e0d8feae28a85c2320172de5540e10bf96966c6f
Ruy Braga Filho <ruybragafilho@gmail.com>
2022-11-08 02:00:24 -0300

    Nova Correcao BoardTest

--- src/test/java/com/chessgame/boardGame/BoardTest.java
+++ src/test/java/com/chessgame/boardGame/BoardTest.java
@@ -10,6 +10,7 @@ import org.junit.After;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 import main.java.com.chessgame.boardGame.Board;
 import main.java.com.chessgame.boardGame.Piece;
